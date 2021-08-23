5a61e185ae8784e2c784e080bcde4f96b200f264
HJW <hju0518@sogang.ac.kr>
2021-08-23 21:02:22 +0900

    Lip

--- Lip_tracking.py
+++ Lip_tracking.py
@@ -15,6 +15,11 @@ from utils import find_q2k, find_outliers
 import pdb
 import pickle
 import skvideo.io
+import json
+from glob import glob
+
+
+
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
 # construct the argument parser and parse the arguments
@@ -23,8 +28,10 @@ ap.add_argument("-v", "--video_path", type=str, required=True,
 	help="path to input video file")
 ap.add_argument("-sv", "--save_video_path", type=str, required=True,
 	help="path to output video file")
-ap.add_argument("-m", "--mode", type=str, default="tracking",
-        help="select detect, tracking or both", )
+ap.add_argument("-sl", "--save_label_path", type=str, required=True,
+	help="path to output video file")
+ap.add_argument("-m", "--mode", type=str,required=True,
+        help="select detect, tracking or detection", )
 ap.add_argument("-t", "--tracker", type=str, default="medianflow",
 	help="OpenCV object tracker type")
 args = vars(ap.parse_args())
@@ -51,28 +58,31 @@ fa_probs_threshold  = 0.95
 #trackers            = cv2.MultiTracker_create()
 
 
-fps=30
+fps=25
 size=(88,88)
-border = 100
+border = 20
+
+
 
 video_list=[x for x in listdir(args["video_path"]) if ".mp4" in x]
 video_list=sorted(video_list)
 video_count=0
-
+f = open("./no_process_videos.txt", 'w')
 for video_name in video_list:
-
     try:
         video=args["video_path"]+str(video_name)
-        out_path = args["save_video_path"] + str(video_name)   
-    
-        if os.path.exists(out_path):
-            print("already existed: ", video_name)
-            continue
+        out_path = args["save_video_path"] + '/'+str(video_name)   
+        label_out_path = args["save_label_path"] +'/Lip_'+ str(video_name)[:-4]+'.json'
         
-        # reader = skvideo.io.FFmpegReader(video)
-        # video_shape = reader.getShape()
-        # (num_frames, h, w, c) = video_shape
-        # print("input_video shape : ",num_frames, h, w, c)
+        if not os.path.exists(args["save_video_path"]):
+            os.makedirs(args["save_video_path"])
+        if not os.path.exists(args["save_label_path"]):
+            os.makedirs(args["save_label_path"])
+        
+        reader = skvideo.io.FFmpegReader(video)
+        video_shape = reader.getShape()
+        (num_frames, h, w, c) = video_shape
+
 
         start_time=time.time()
         vs = cv2.VideoCapture(video)
@@ -88,14 +98,11 @@ for video_name in video_list:
             hasFrame, frame = vs.read()
             if not hasFrame:
                 break
-
             count+=1      
             if args["mode"] == "tracking":
                 ############################ face detect at first frame ############################
-                if n_frame == 0:
-                    bbox = []
+                if n_frame == 0:                   
                     pred, probs = fa.get_landmarks(frame)
-                    # pdb.set_trace()
                     if len(probs) > 1:
                         for prob in probs:
                             overlapped_list.append(prob)
@@ -124,10 +131,29 @@ for video_name in video_list:
                     for i in range(len(boxes)):
                         box.append(int(boxes[i]))
                     box=tuple(box)
-        
-            else:
-                print("NotImplementMode Error")
-                sys.exit()
+
+            if args["mode"] == "detection":       
+                pred, probs = fa.get_landmarks(frame)
+                if len(probs) > 1:
+                    for prob in probs:
+                        overlapped_list.append(prob)
+                    min_index=overlapped_list.index(max(overlapped_list))
+                    pred=[pred[min_index]]
+                    overlapped_list=[]
+                
+                pred = np.squeeze(pred)
+                x = pred[48:,0]
+                y = pred[48:,1]
+                min_x = min(x)-border
+                min_y = min(y)-border
+                max_x = max(x)+border
+                max_y = max(y)+border
+                height = int((max_y-min_y)/2)
+                width = int((max_x-min_x)/2)
+                standard=max(height,width)
+                box = [int(min_x), int(min_y), int(max_x), int(max_y)]
+                box = tuple(box)
+
 
             (x, y, w, h) = [int(v) for v in box]
             
@@ -139,12 +165,31 @@ for video_name in video_list:
             
             crop_img = frame[left_boundary:right_boundary,top_boundary:bottom_boundary]
             resized_crop_img=cv2.resize(crop_img, dsize=size,interpolation=cv2.INTER_LINEAR)
-            files.append(resized_crop_img) 
+            files.append(resized_crop_img)
             n_frame += 1
         
            
         print("mpg vs mpg_crop: {} vs {}".format(count,len(files)))
-
+        
+        if num_frames == len(files):
+            print("Good crop: ", video)
+            lip_box = dict()
+            for i in range(num_frames):
+                lip_box['frame_'+str(i)]={}
+                lip_box['frame_'+str(i)]['xtl']=left_boundary
+                lip_box['frame_'+str(i)]['ytl']=top_boundary
+                lip_box['frame_'+str(i)]['xbl']=right_boundary
+                lip_box['frame_'+str(i)]['ybl']=bottom_boundary
+        else:
+            print("No crop: ", video)
+            f.write(video)
+            f.write('\n')
+            continue
+        
+        with open(label_out_path, 'w', encoding='utf-8') as make_file:
+            json.dump(lip_box, make_file, indent="\t")
+        
+        
         out = cv2.VideoWriter(
                 out_path,
                 #cv2.VideoWriter_fourcc(*'DIVX'),
@@ -162,6 +207,7 @@ for video_name in video_list:
         vs.release()
         print(video_name, " saved",video_count)
         print("time: ",time.time()-start_time) 
+        f.close()
     except Exception as ex:
         print("ERROR:",ex,video_name)
 
